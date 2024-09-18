675e54e138f94422f42a0dc469b2531d3a5722a9
Vladyslav Nosal <vladyslavnosal@gmail.com>
2024-09-17 21:08:40 -0700

    Implement hook for controlling behavior of body element

---
+++ frontend/src/hooks/useBody.ts
@@ -0,0 +1,18 @@
+export const useBody = () => {
+  const preventScroll = () => {
+
+    const initialHeight = document.body.style.height;
+    const initialOverflow = document.body.style.overflowY;
+
+
+    document.body.style.height = "100vh";
+    document.body.style.overflowY = "hidden";
+
+    return () => {
+      document.body.style.height = initialHeight;
+      document.body.style.overflowY = initialOverflow;
+    }
+  }
+
+  return { preventScroll}
+}
