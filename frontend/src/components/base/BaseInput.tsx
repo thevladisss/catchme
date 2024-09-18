70aebe871a28592ed3c5810a0dafae75d53aaf81
Vladyslav Nosal <vladyslavnosal@gmail.com>
2024-09-17 22:11:36 -0700

    Add BaseInput component

---
+++ frontend/src/components/base/BaseInput.tsx
@@ -0,0 +1,7 @@
+import {TextField, TextFieldProps} from "@mui/material";
+
+type BaseInputProps = TextFieldProps & {}
+export default function BaseInput(props: BaseInputProps) {
+
+  return <TextField {...props}></TextField>
+}
