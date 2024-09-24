4987ccf3c2f65204a67daf5376ba71ee946599d4
Vladyslav Nosal <vladyslavnosal@gmail.com>
2024-09-23 21:13:28 -0700

    Update props declaration on BaseDialog and BaseButton components.

--- frontend/src/components/base/BaseButton.tsx
+++ frontend/src/components/base/BaseButton.tsx
@@ -1,21 +1,33 @@
 import * as React from "react";
 import { Button, ButtonProps } from "@mui/material";
 import { mergeClasses } from "../../utils/mergeClasses";
+import { useMemo } from "react";
 
 type BaseButtonProps = React.HTMLProps<any> &
-  { children: React.ReactNode} &
-  ButtonProps & {
-    fontSize?: string;
-    width?: string;
-    height?: string;
-  };
+  React.PropsWithChildren<
+    ButtonProps & {
+      fontSize?: string;
+      width?: string;
+      height?: string;
+      square?: boolean;
+    }
+  >;
 
 const defaultProps = {
   color: "primary",
 };
 
 export default function BaseButton(props: BaseButtonProps) {
-  const { fontSize, width, height, children, ...passThroughProps } = props;
+  const { square, fontSize, width, height, children, ...passThroughProps } =
+    props;
 
-  return <Button {...passThroughProps}>{children}</Button>;
+  const styles = useMemo(() => {
+    return Object.assign({}, square ? { borderRadius: 0 } : {});
+  }, [square, fontSize, height, width]);
+
+  return (<>
+    <Button style={styles} {...passThroughProps}>
+      {children}
+    </Button>
+  </>);
 }
--- frontend/src/components/base/BaseDialog.tsx
+++ frontend/src/components/base/BaseDialog.tsx
@@ -2,8 +2,7 @@ import { Dialog, DialogProps } from "@mui/material";
 import * as React from "react";
 
 type BaseDialogProps = React.HTMLProps<any> &
-  Partial<{ children: React.ReactNode; }> &
-  DialogProps & {};
+  React.PropsWithChildren<DialogProps & {}>
 
 export default function BaseDialog(props: BaseDialogProps) {
   return <Dialog {...props}>{props.children}</Dialog>;
